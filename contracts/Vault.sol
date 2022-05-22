180d4a1aa08a4d7c775c7cfa56c3a063207082c9
Itay Brenner <itay@itaysoft.com>
2022-05-22 01:40:09 -0300

    Implement logic to add and remove admins in Vault

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,10 +4,37 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Vault {
+    uint256 private administratorsCount;
+    mapping(address => bool) private administrators;
+
+    modifier onlyAdmin() {
+        require(administrators[msg.sender], "User must be administrator to perform this operation");
+        _;
+    }
+
     constructor() {
+        administratorsCount = 1;
+        administrators[msg.sender] = true;
     }
 
     function isAContract() public pure returns (bool) {
         return true;
+    }
+
+    function isAdmin(address _admin) external view returns (bool) {
+        return administrators[_admin];
+    }
+
+    function addAdmin(address _admin) external onlyAdmin {
+        require(!this.isAdmin(_admin), "Account is already an admin");
+        administratorsCount += 1;
+        administrators[_admin] = true;
+    }
+
+    function removeAdmin(address _admin) external onlyAdmin {
+        require(this.isAdmin(_admin), "Account is not an admin");
+        require(administratorsCount > 1, "There must be at least one admin");
+        administratorsCount -= 1;
+        administrators[_admin] = false;
     }
 }
--- test/vault-test.js
+++ test/vault-test.js
@@ -1,12 +1,71 @@
-const { expect } = require("chai");
+const { expect, use } = require("chai");
 const { ethers } = require("hardhat");
+const { solidity } = require("ethereum-waffle");
 
-describe("Vault", function () {
-  it("is a contract", async function () {
-    const Vault = await ethers.getContractFactory("Vault");
-    const contract = await Vault.deploy();
-    await contract.deployed();
+use(solidity);
 
-    expect(await contract.isAContract()).to.be.true;
+// Contract instance variable
+let provider, signer, account1, account2, deployedContractInstance;
+
+// Constant
+const contractName              = "Vault";
+
+describe(contractName, function () {
+  before(async() => {
+    console.log("------------------------------------------------------------------------------------");
+    console.log("--", contractName, "Contract Test Start");
+    console.log("------------------------------------------------------------------------------------"); 
+
+    // Get provider and Signer
+    provider = ethers.provider;
+    [signer, account1, account2] = await ethers.getSigners();
+
+    // Deploy contract
+    const contractPath          = "contracts/" + contractName + ".sol:" + contractName;
+    const contractFactory       = await ethers.getContractFactory(contractPath, signer, );
+    deployedContractInstance    = await contractFactory.deploy();
+  });
+
+  it("Deployer is admin", async() => {
+    const isAdmin = await deployedContractInstance.isAdmin(signer.address);
+    expect(isAdmin).to.be.true;
+  });
+
+  it("Non admin cannot add another admin", async() => {
+    const operation = deployedContractInstance.connect(account1).addAdmin(account2.address);
+    await expect(operation).to.be.revertedWith('User must be administrator to perform this operation');
+  });
+
+  it("One Admin can set another user as admin", async() => {
+    await deployedContractInstance.connect(signer).addAdmin(account1.address);
+
+    const isAdmin = await deployedContractInstance.isAdmin(account1.address);
+    expect(isAdmin).to.be.true;
+  });
+
+  it("Admin cannot add an admin again", async() => {
+    await expect(deployedContractInstance.addAdmin(signer.address)).to.be.revertedWith('Account is already an admin');
+  });
+
+  it("Non admin cannot remove admin", async() => {
+    const operation = deployedContractInstance.connect(account2).removeAdmin(signer.address);
+    await expect(operation).to.be.revertedWith('User must be administrator to perform this operation');
+  });
+
+  it("Trying to remove non admin reverses", async() => {
+    const operation = deployedContractInstance.connect(account1).removeAdmin(account2.address);
+    await expect(operation).to.be.revertedWith('Account is not an admin');
+  });
+
+  it("Admin can remove another admin", async() => {
+    await deployedContractInstance.connect(account1).removeAdmin(signer.address);
+    
+    const isAdmin = await deployedContractInstance.isAdmin(signer.address);
+    expect(isAdmin).to.be.false;
+  });
+
+  it("Admin cannot remove himself if he is the only admin", async() => {
+    await expect(deployedContractInstance.connect(account1).removeAdmin(account1.address))
+      .to.be.revertedWith('There must be at least one admin');
   });
 });
