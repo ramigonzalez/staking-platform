bcc631150f9fa0b30d65e4aee51f553183f7659a
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-05-24 19:08:04 -0300

    format applied

--- contracts/TokenContract.sol
+++ contracts/TokenContract.sol
@@ -1,11 +1,10 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.9;
 
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 contract TokenContract {
-    constructor() {
-    }
+    constructor() {}
 
     function isAContract() public pure returns (bool) {
         return true;
