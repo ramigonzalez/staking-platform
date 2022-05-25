92c54bc68a97488df5c3e27dc48dba9a7f12cc7c
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-05-25 11:18:52 -0300

    compile test

--- test/token-test.js
+++ test/token-test.js
@@ -1,12 +1,12 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("TokenContract", function () {
-  it("is a contract", async function () {
-    const TokenContract = await ethers.getContractFactory("TokenContract");
-    const contract = await TokenContract.deploy();
-    await contract.deployed();
+describe('TokenContract', function () {
+    it('is a contract', async function () {
+        const TokenContract = await ethers.getContractFactory('TokenContract');
+        const contract = await TokenContract.deploy(10000);
+        await contract.deployed();
 
-    // expect(await contract.isAContract()).to.be.true;
-  });
+        // expect(await contract.isAContract()).to.be.true;
+    });
 });
