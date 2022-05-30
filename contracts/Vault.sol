b2a8a606d5df692020fa043a7f01bd97e9c560a2
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-05-30 19:38:59 -0300

    sell & buy price

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,7 +4,15 @@ pragma solidity 0.8.9;
 import 'hardhat/console.sol';
 
 contract Vault {
+    
     uint256 private administratorsCount;
+    
+    /**
+     * Can be changed to [external] to optimize GAS USED
+     */
+    uint256 public sellPrice;
+    uint256 public buyPrice;
+    
     mapping(address => bool) private administrators;
 
     modifier onlyAdmin() {
@@ -41,5 +49,18 @@ contract Vault {
         require(administratorsCount > 1, "There must be at least one admin");
         administratorsCount -= 1;
         delete administrators[_admin];
+    }
+
+    function setSellPrice(uint256 _sellPrice) external {
+        require(_sellPrice > 0, 'Sell price must be greater than 0');
+        require(_sellPrice > buyPrice, 'Sell price must be greater than buy price');
+        sellPrice = _sellPrice;
+    }
+
+    function setBuyPrice(uint256 _buyPrice) external {
+        require(_buyPrice > 0, 'Buy price must be greater than 0');
+        require(sellPrice > 0, 'Sell price must be set first');
+        require(_buyPrice < sellPrice, 'Buy price must be lower than sell price');
+        buyPrice = _buyPrice;
     }
 }
