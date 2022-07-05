9762f76092e16058e6d104acc9c06eca35472b23
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-07-05 00:14:32 -0300

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,59 +1,76 @@
 const { ethers } = require('hardhat');
-const fs = require('fs');
-const path = require('path');
+const { contractABI, deployContract, toEthers, waitTransactionConfirmations } = require('./utils');
 
 // Contract to deploy
-const contractToDeploy = 'MyContract';
+const contractName = {
+    tokenContract: 'TokenContract',
+    vault: 'Vault',
+    farm: 'Farm',
+};
+
+const protocolDeploy = {
+    tokenContractAbi: contractABI(contractName.tokenContract),
+    vaultAbi: contractABI(contractName.vault),
+    farmAbi: contractABI(contractName.farm),
+};
+
+const INITIAL_AMOUNT = toEthers(10);
 
 async function main() {
-    // Get provider. Connect with default hardhat provider
-    const provider = ethers.provider;
-
     // Get provider for testnet
-    // const accessPoint_URL = process.env.GANACHE_ACCESSPOINT_URL;
-    // const provider = new ethers.providers.JsonRpcProvider(accessPoint_URL);
+    const accessPoint_URL = process.env.RINKEBY_ACCESSPOINT_URL;
+    const provider = new ethers.providers.JsonRpcProvider(accessPoint_URL);
 
     // Get signer
-    const [signer] = await ethers.getSigners();
+    const [deployer] = await ethers.getSigners();
+
+    console.log('Deploying protocol with the account:', deployer.address);
+
+    let deployerBalance = ethers.utils.formatEther(await deployer.getBalance());
+    console.log('---------------------------------------------------------------------------------------');
+    console.log('-- Signer address:\t', deployer.address);
+    console.log('-- Signer balance:\t', deployerBalance);
+    console.log('---------------------------------------------------------------------------------------');
 
     // Get Contracts to deploy
-    const contractPath = 'contracts/' + contractToDeploy + '.sol:' + contractToDeploy;
-    const contractFactory = await ethers.getContractFactory(contractPath, signer);
-    const deployedContract = await contractFactory.deploy();
+    const vaultContract = await deployContract(deployer, protocolDeploy.vaultAbi);
+    await waitTransactionConfirmations(vaultContract, contractName.vault, provider);
 
-    // Check transaction result. 1 it is the number of transction to wait
-    tx_hash = deployedContract.deployTransaction.hash;
-    const confirmations_number = 1;
-    tx_result = await provider.waitForTransaction(tx_hash, confirmations_number);
-    if (tx_result.confirmations < 0 || tx_result === undefined) {
-        throw new Error(contractToDeploy || ' Contract ERROR: Deploy transaction is undefined or has 0 confirmations.');
-    }
+    console.log('---------------------------------------------------------------------------------------');
+    console.log('-- Deployed contract:\t', contractName.vault);
+    console.log('-- Contract address:\t', vaultContract.address);
+    console.log('---------------------------------------------------------------------------------------');
 
-    // Get contract read only instance
-    const contractABIPath =
-        path.resolve(process.cwd(), 'artifacts/contracts/', contractToDeploy) + '.sol/' + contractToDeploy + '.json';
-    const contractArtifact = JSON.parse(fs.readFileSync(contractABIPath, 'utf8'));
-    const deployedContractInstance = new ethers.Contract(deployedContract.address, contractArtifact.abi, provider);
-    const signerBalance = ethers.utils.formatEther(await signer.getBalance());
+    const tokenContract = await deployContract(deployer, protocolDeploy.tokenContractAbi, [INITIAL_AMOUNT]);
+    await waitTransactionConfirmations(tokenContract, contractName.tokenContract, provider);
 
-    // Check getVersion function
-    const contractVersion = await deployedContractInstance.VERSION();
-    if (contractVersion != 100) {
-        throw new Error(`-- ${contractToDeploy} contract ERROR: Version check fail.`);
-    } else {
-        console.log('');
-        console.log('---------------------------------------------------------------------------------------');
-        console.log('-- Deployed contract:\t', contractToDeploy);
-        console.log('---------------------------------------------------------------------------------------');
-        console.log('-- Contract address:\t', deployedContractInstance.address);
-        console.log('-- Contract version:\t', parseInt(contractVersion));
-        console.log('---------------------------------------------------------------------------------------');
-        console.log('-- Signer address:\t', signer.address);
-        console.log('-- Signer balance:\t', signerBalance);
-        console.log('---------------------------------------------------------------------------------------');
-        console.log('-- Deploy successfully');
-        console.log('---------------------------------------------------------------------------------------');
-    }
+    console.log('---------------------------------------------------------------------------------------');
+    console.log('-- Deployed contract:\t', contractName.tokenContract);
+    console.log('-- Contract address:\t', tokenContract.address);
+    console.log('---------------------------------------------------------------------------------------');
+
+    const farmContract = await deployContract(deployer, protocolDeploy.farmAbi, [tokenContract.address, vaultContract.address]);
+    await waitTransactionConfirmations(farmContract, contractName.farm, provider);
+
+    console.log('---------------------------------------------------------------------------------------');
+    console.log('-- Deployed contract:\t', contractName.farm);
+    console.log('-- Contract address:\t', farmContract.address);
+    console.log('---------------------------------------------------------------------------------------');
+
+    console.log('');
+    console.log('---------------------------------------------------------------------------------------');
+    console.log('-- Token name:\t', await tokenContract.name());
+    console.log('-- Token decimals:\t', await tokenContract.decimals());
+    console.log('---------------------------------------------------------------------------------------');
+    console.log('-- Successfully protocol deploy');
+    console.log('---------------------------------------------------------------------------------------');
+
+    console.log('');
+    deployerBalance = ethers.utils.formatEther(await deployer.getBalance());
+    console.log('---------------------------------------------------------------------------------------');
+    console.log('-- Signer address:\t', deployer.address);
+    console.log('-- Signer balance:\t', deployerBalance);
+    console.log('---------------------------------------------------------------------------------------');
 }
 
 main()
