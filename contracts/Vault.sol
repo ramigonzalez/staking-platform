3e95fb169440d2d3d30089d55568144dacdc1038
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-07-05 01:55:49 -0300

    format

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -107,7 +107,7 @@ contract Vault {
         tokenContract = ERC20Interface(_tokenContractAddress);
     }
 
-    function setFarmAddress (address _farmAddress) external isValidAddress(_farmAddress) {
+    function setFarmAddress(address _farmAddress) external isValidAddress(_farmAddress) {
         farmAddress = _farmAddress;
     }
 
